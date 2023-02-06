fb5ee523d051ed84fa681b101bcf5c258e2bae6f
Alex Biehl <ab7289@nyu.edu>
2023-02-06 18:54:45 -0500

    formatted files

--- csgy9223a_hw1/src/main/java/cloud_lex_chatbot/App.java
+++ csgy9223a_hw1/src/main/java/cloud_lex_chatbot/App.java
@@ -4,10 +4,8 @@ package cloud_lex_chatbot;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
