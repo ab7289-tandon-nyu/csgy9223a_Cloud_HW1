ef555f786b44f568a51551c2811cd2f172fff94c
Alex Biehl <ab7289@nyu.edu>
2023-02-13 18:49:30 -0500

    finally got lambda to return hello world

--- lambdas/lf0/lambda_function.py
+++ lambdas/lf0/lambda_function.py
@@ -1,47 +1,44 @@
 import json
-import uuid
-import json
 from datetime import datetime
+from uuid import uuid4
 
-class Error():
-    def __init__(self, code, message):
-        self.code = code
-        self.message = message
+def create_error(code: int, message: str) -> dict:
+    return dict(
+        code=code,
+        message=message
+    )
 
-class UnstructuredMessage():
-    def __init__(self, id, text, timestamp):
-        self.id = id
-        self.text = text
-        self.timestamp = timestamp
-        
-class Message():
-    def __init__(self, unstructured, type: str = "unstructured"):
-        self.type = type
-        self.unstructured = unstructured
-        
-class BotResponse():
-    def __init__(self, messages):
-        self.messages = messages
-    
-    def toJson(self):
-        pass
+def create_unstructured_message(id, text, timestamp):
+    return dict(
+        id=id,
+        text=text,
+        timestamp=timestamp
+    )
+
+def create_message(u_message, type: str = "unstructured"):
+    message = dict()
+    message["type"] = type
+    message["unstructured"] = u_message
+    return message   
+
+def create_bot_response(messages: list):
+    return dict(
+        messages=messages
+    )        
         
 def create_simple_message(msg_text: str):
     dt = datetime.now()
     ts = datetime.timestamp(dt)
-    unstructured = UnstructuredMessage(
-        str(uuid.uuid4()),
+    unstructured = create_unstructured_message(
+        str(uuid4()),
         msg_text,
         ts)
     
-    message = Message(unstructured)
-    return BotResponse([ message ])
+    message = create_message(unstructured)
+    bot_response = create_bot_response([message])
+    return bot_response
 
 def lambda_handler(event, context):
-    resp = create_simple_message("Hello World!")
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!'),
-        'event': event,
-        'data': json.dumps(resp.__dict__)
-    }
+    resp: dict = create_simple_message("Hello World!")
+    
+    return resp
