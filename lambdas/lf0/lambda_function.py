e33d80e1f9454fc93837f48c8fd648246871d2ba
Alex Biehl <ab7289@nyu.edu>
2023-02-24 17:41:21 -0500

    updated lf0 lambda to post to lex

--- lambdas/lf0/lambda_function.py
+++ lambdas/lf0/lambda_function.py
@@ -1,6 +1,7 @@
 import json
 from datetime import datetime
 from uuid import uuid4
+import boto3
 
 def create_error(code: int, message: str) -> dict:
     return dict(
@@ -37,8 +38,40 @@ def create_simple_message(msg_text: str):
     message = create_message(unstructured)
     bot_response = create_bot_response([message])
     return bot_response
+    
+def parse_response(response: dict):
+    metadata = response["ResponseMetadata"]
+    http_status = metadata["HTTPStatusCode"]
+    # if http_status == 200:
+    #     messages = response['messages']
+    # else:
+    messages = response['messages']
+    return messages[0]['content']
+        
+    
+def post_to_bot(event):
+    client = boto3.client('lexv2-runtime')
+    
+    msg: str = event['messages'][0]['unstructured']['text']
+    print(f"Parsed message: {msg}")
+    
+    response = client.recognize_text(
+        botId='GV9YXEA5BJ',
+        botAliasId='VEY7QVDD0A',
+        localeId='en_US',
+        sessionId='testsession',
+        text=msg,
+    )
+    
+    return parse_response(response)
+    
+    print(f"received response from lex: {response}")
 
 def lambda_handler(event, context):
-    resp: dict = create_simple_message("Hello World!")
+    print(f"event: {event}")
+    print(f"context: {context}")
+    rsp_msg: str = post_to_bot(event)
+    # resp: dict = create_simple_message("Hello World!")
+    resp: dict = create_simple_message(rsp_msg)
     
     return resp
